d11c9f04fb2edc5a6b62a2eec03732d914d49eeb
Christopher Albert <albert@alumni.tugraz.at>
2024-02-14 20:36:02 +0100

    Main routine

--- src/dat/dat.py
+++ src/dat/dat.py
@@ -1,25 +1,8 @@
-# TODO: Write main script
+from sys import argv
 
 def main():
-    command = argv[1]
-    if command == "log":
-        create_experiment(code = argv[2], title = argv[3])
-    else:
-        print("Usage: journal.py log <code> <title>")
-
-
-def create_experiment(code, title):
-    experiment = manager.create_experiment()
-    content = {"title": title, "metadata": json.dumps({
-               "extra_fields": {
-                   "code": {
-                        "type": "text",
-                        "value": code
-                   }
-               }})
-               }
-    print(manager.post_experiment(experiment["id"], content))
-
+    print("Hello, world!")
+    print(argv)
 
 if __name__ == "__main__":
     main()
