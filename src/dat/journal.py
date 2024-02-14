d0479106a444b2fae10d1a5919d29a39b5334b57
Christopher Albert <albert@alumni.tugraz.at>
2024-02-14 16:05:40 +0000

    JSON storage and HDF5 stub

--- src/dat/journal.py
+++ src/dat/journal.py
@@ -6,7 +6,6 @@ See https://alexgu2008.github.io/elabftw_api_support/
 import elabapy
 import json
 import os
-from sys import argv
 
 def create_entry(title):
     entry = {}
@@ -17,12 +16,14 @@ def copy_fields(from_entry, to_entry):
     for key in from_entry:
         to_entry[key] = from_entry[key]
 
+
 class Storage:
     def load(self, entry):
         pass
 
     def save(self, entry):
         pass
+
 
 class ElabStorage(Storage):
     def __init__(self):
@@ -46,3 +47,27 @@ class ElabStorage(Storage):
             self.manager.post_experiment(entry["id"], entry)
         else:
             raise ValueError("entry['id'] is empty. Use create().")
+
+
+class HDF5Storage(Storage):
+    def __init__(self, filename):
+        self.filename = filename
+
+    def load(self, entry):
+        raise NotImplementedError
+
+    def save(self, entry):
+        raise NotImplementedError
+
+
+class JSONStorage(Storage):
+    def __init__(self, filename):
+        self.filename = filename
+
+    def load(self):
+        with open(self.filename, "r") as f:
+            return json.load(f)
+
+    def save(self, entry):
+        with open(self.filename, "w") as f:
+            json.dump(entry, f)
