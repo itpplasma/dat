a5be9c25e72b1834045303a13c508473d8ec1245
Christopher Albert <albert@alumni.tugraz.at>
2024-02-15 07:00:48 +0100

    argv

--- src/dat/dat.py
+++ src/dat/dat.py
@@ -1,8 +1,19 @@
 from sys import argv
 
 def main():
-    print("Hello, world!")
-    print(argv)
+    print("This is Dat.")
+    print("Arguments:", argv)
+    command = get_function_by_name(argv[1])
+    command()
+
+
+def get_function_by_name(name):
+    return globals()[name]
+
+
+def commit():
+    print("Committing changes")
+
 
 if __name__ == "__main__":
     main()
